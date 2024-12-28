60e6f598a72be124b470130fc5e9a0d08a14b3d8
Duckteam <69460253+RealDuckteam@users.noreply.github.com>
2024-12-28 18:56:23 +0100

    Delete dtslo/lua directory

--- dtslo/lua/autorun/dtslo_init.lua
+++
@@ -1,8 +0,0 @@
-if SERVER then
-    AddCSLuaFile("lua/dtslo/config.lua")
-    AddCSLuaFile("lua/client/dtslo_client.lua")
-    include("lua/server/dtslo_server.lua")
-else
-    include("lua/dtslo/config.lua")
-    include("lua/client/dtslo_client.lua")
-end
--- dtslo/lua/client/dtslo_client.lua
+++
@@ -1,8 +0,0 @@
-net.Receive("DTSLO_OpenURL", function()
-    local url = net.ReadString()
-    gui.OpenURL(url)
-
-    if DTSLO_Config.SoundFile then
-        surface.PlaySound(DTSLO_Config.SoundFile)
-    end
-end)
